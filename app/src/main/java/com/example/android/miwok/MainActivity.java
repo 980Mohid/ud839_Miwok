810b0fb65aa36234b6b1a2dd72b38f28ea94267e
SamBread <980mohid@gmail.com>
2017-12-13 13:24:31 +0500

    Revert "Starter code"
    This reverts commit a6df7e73966c65594ae4c35c89b3e691d0fe9c44.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,20 +15,23 @@
  */
 package com.example.android.miwok;
 
+<<<<<<< HEAD
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+=======
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+>>>>>>> parent of a6df7e7... Starter code
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
         // Find the View that shows the numbers category
